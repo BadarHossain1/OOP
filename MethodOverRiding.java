2c5b585c6f502d4a45fbbf7e3dabf68f4cfa91cb
Badar Hossain <badar12041@gmail.com>
2024-07-17 17:24:03 +0600

    Method overriding in Java

---
+++ MethodOverRiding.java
@@ -0,0 +1,31 @@
+public class MethodOverRiding {
+    public static void main(String[] args){
+        //method overRiding is Declaring a method in sub class which is already present in parent class , done so that a child can give its own implementation.
+
+        Dog dog = new Dog();
+        dog.sound();
+
+        Animal animal = new Animal();
+        animal.sound();
+
+
+
+
+    }
+}
+
+
+class Animal{
+    void sound(){
+        System.out.println("Animal is making a sound");
+    }
+}
+
+class Dog extends Animal{
+
+    @Override
+    //over written the method it inherits from its parent class. 
+    void sound(){
+        System.out.println("The dog got the dawg in him");
+    }
+}
