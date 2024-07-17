f6015928187b8e7e9c079fc038237c5928b40370
Badar Hossain <badar12041@gmail.com>
2024-07-17 17:17:28 +0600

    Inheritance added

--- Inheritance.java
+++ Inheritance.java
@@ -1,3 +1,39 @@
 public class Inheritance {
-    
+    public static void main(String[] args){
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.go();
+        car.stop();
+
+        bicycle.go();
+        bicycle.stop();
+        System.out.println(bicycle.wheels);
+    }
+}
+
+class Vehicle{ //vehicle class is super class or ancestor
+    double speed;
+    void go(){
+        System.out.println("This vehicle is broom broom");
+    }
+
+    void stop(){
+        System.out.println("This vehicle is stopped");
+    }
+}
+
+class Car extends Vehicle{
+    //Inherits from Vehicle
+
+    int wheels = 4;
+    int doors = 4;
+
+}
+
+class Bicycle extends Vehicle{
+    //Inherits from Bicycle
+
+    int wheels = 2;
+    int pedals = 2;
 }
