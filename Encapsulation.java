9cf66c3cb361a70736038138173c740d704ac6b0
Badar Hossain <badar12041@gmail.com>
2024-09-26 12:11:18 +0600

    Encapsulation theory

--- Encapsulation.java
+++ Encapsulation.java
@@ -2,4 +2,13 @@ public class Encapsulation {
     //Wrapping up the implementation of the data members and methods in a class. 
 
     //Encapsulation is a sub part of data hiding. Data hiding is protecting information and its always private. It is used to hide the complexity of the system. Encapsulation can be public or private. One example can be declaring an ArrayList. We can use getters and setters which is encapsulation, but the array that we are making should be private and that is data hiding. 
+
+
+    //More Theoretical Concept given by Jalal Sir
+
+    // #How to Implement Encapsulation?
+    // => We need to perform two steps to achieve the purpose of encapsulation:
+        // * Use the private access modifier to all variables/fields of class as private.
+        // * Define public setter and getter methods to read and modify/set the value
+
 }
--- Final.java
+++ Final.java
@@ -3,7 +3,7 @@ public class Final {
         final int a = 10;
         // a = 20; // cant change the value of a because of final keyword and also a is primitive data type
 
-        // @Override //this is called annotation and it is used to check if its overriding something or not like area is overriding the parent area. But when final keyword is used , it cant be overridden. And it helps the compiler to run things faster when final is used. This is called Early Binding. Late binding is when at run time the compiler does dynamic method resolution when the program is running and choosing which to run. Also when you use final in a class, inclusively it makes the methods in it final too, thats why you cant extend that class. 
+        // @Override //this is called annotation and it is used to check if its overriding something or not like area is overriding the parent area. But when final keyword is used , it cant be overridden. And it helps the compiler to run things faster when final is used. This is called Early Binding. Late binding is when at run time the compiler does dynamic method resolution when the program is running and choosing which to run. Also when you use final in a class, inclusively it makes the methods in it final too, thats why you cant extend that class. You can inherit the methods but cant override them because of using final. Also final classes cannot be inherited. 
         
 
         final Student student = new Student(null, null, null);
