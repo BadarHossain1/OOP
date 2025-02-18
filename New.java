553b493a0d30f60e38bebfe91029ed0db525ccf7
Badar Hossain <badar12041@gmail.com>
2025-02-18 10:03:06 +0600

    New keyword

--- New.java
+++ New.java
@@ -1,6 +1,6 @@
 public class New {
     public static void main(String[] args){
-        Student student1 = new Student("Badar", "125", "3.90");
+        Student student1 = new Student("Badar", "125", "3.90"); // new keyword decides the memory allocation in heap memory and returns the reference to the object through the operating system.
         Student copy = student1;
 
         student1.name = "Badar Hossain"; // change in student1 changes the copy instance
